d85f26f65897b1e945526227491a83f5c3cae62b
EC2 Default User <ec2-user@ip-172-31-31-25.us-west-2.compute.internal>
2023-11-22 10:06:19 +0000

    working

--- README.md
+++ README.md
@@ -10,7 +10,13 @@ docker build -t flashcard_llm .
 docker run --rm -e AWS_LAMBDA_FUNCTION_TIMEOUT=900 -e AWS_LAMBDA_FUNCTION_MEMORY_SIZE=10240 -p 9000:8080 flashcard_llm:latest
 ```
 
+# clean docker
+```
+docker system prune -f -a
+```
+
 # how to test function
 ```
+curl "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{"max_tokens": "1024", "prompt": "[\"hello\", \"good morning\", \"I\", \"like\", \"eat\", \"drink\", \"rice\", \"water\"]"}'
 curl "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{"max_tokens": "1024", "prompt": "<s>[INST] <<SYS>> You are a helpful assistant. <</SYS>> What is a Large Language Model? [/INST]"}'
 ```
--- app.py
+++ app.py
@@ -1,12 +1,73 @@
-from llama_cpp import Llama
+from llama_cpp.llama import Llama, LlamaGrammar
+import json
+import logging  
 import os
 
 MODEL_NAME = os.environ['MODEL_NAME']
-
 llm = Llama(model_path=f"./model/{MODEL_NAME}")
+logger = logging.getLogger()
+
+grammar_text = r'''
+root   ::= object
+value  ::= object | array | string | number | ("true" | "false" | "null") ws
+
+object ::=
+  "{" ws (
+            string ":" ws value
+    ("," ws string ":" ws value)*
+  )? "}" ws
+
+array  ::=
+  "[" ws (
+            value
+    ("," ws value)*
+  )? "]" ws
+
+string ::=
+  "\"" (
+    [^"\\] |
+    "\\" (["\\/bfnrt] | "u" [0-9a-fA-F] [0-9a-fA-F] [0-9a-fA-F] [0-9a-fA-F]) # escapes
+  )* "\"" ws
+
+number ::= ("-"? ([0-9] | [1-9] [0-9]*)) ("." [0-9]+)? ([eE] [-+]? [0-9]+)? ws
+
+# Optional space: by convention, applied in this grammar after literal chars when allowed
+ws ::= ([ \t\n] ws)?
+'''
+grammar = LlamaGrammar.from_string(grammar_text)
+
+def validate_input(input_string):
+    print("input_str: " + input_string)
+    try:
+        # Attempt to parse the input string as a JSON list
+        words = json.loads(input_string)
+        return True
+    except json.JSONDecodeError as e:
+        logger.info("invalid_json=\"{}\"".format(e))
+        # Input is not a valid JSON list
+        return False
 
 def handler(event, context):
     prompt = event['prompt']
+    logger.info("prompt=\"{}\"".format(prompt))
+
+    if not validate_input(prompt):
+        return "{\"error\": \"invalid input\"}"
+
+    llm_input = "You are an english teacher. I will give you words to your word bank which you will use to create phrases and sentences to help me learn the language. Your word bank contains: `{}`. Please output a JSON with the `output` as your key containing 4 sentences and phrases which only use the words in your word bank.".format(prompt)
+
+
     max_tokens = int(event['max_tokens'])    
-    output = llm(prompt, max_tokens=max_tokens, echo=True)
-    return output
+
+    output = llm(llm_input, max_tokens=max_tokens, echo=True)
+    response = llm(
+        llm_input,
+        grammar=grammar, max_tokens=-1
+    )
+
+    text_result = response["choices"][0]["text"]
+    data = json.loads(text_result)
+    data_dump = json.dumps(data, indent=4)
+    logger.info("output=\"{}\"".format(data_dump))
+
+    return  data_dump
